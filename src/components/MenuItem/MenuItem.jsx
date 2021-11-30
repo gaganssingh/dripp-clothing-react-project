3d14e0c183305f928bd0a2ede91e06f1508e372e
Gagan Singh <gagansingh.dev@outlook.com>
2021-11-30 12:01:08 -0500

    Created directory structure, added separatepages & components, and styles them

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,16 @@
+import "./MenuItem.styles.scss";
+
+export default function MenuItem({ title, imageUrl, size }) {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import Directory from "../../components/Directory/Directory";
+import "./HomePage.styles.scss";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
