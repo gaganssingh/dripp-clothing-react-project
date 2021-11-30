ba7af2f1fb63f5f38b428d5ed3a92f1ca659e54e
Gagan Singh <gagansingh.dev@outlook.com>
2021-11-30 11:28:36 -0500

    Added HomePage and styles

---
+++ src/HomePage.jsx
@@ -0,0 +1,40 @@
+import "./HomePage.styles.scss";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOP</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtitle">SHOP</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
