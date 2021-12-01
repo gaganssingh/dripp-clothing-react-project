4bd762236c9a92d16a896a96283a1b2f9633191d
Gagan Singh <gagansingh.dev@outlook.com>
2021-12-01 17:50:50 -0500

    Added the ability to store the user info on firebase upon login

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,38 @@ const firebaseConfig = {
   appId: "1:392487425807:web:537516722bf4c0564ed07a",
 };
 
+// CHECK IF USER ALREADY EXISTS ON FIREBASE
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // IF USER SIGNED OUT, & userAuth OBJ CHANGES TO null, DO NOTHING
+  if (!userAuth) {
+    return;
+  }
+
+  // Check if data for the loggedin user exists on firebase
+  // This could be shopping cart items from a prev visit etc.
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  // If user data doesn't exist on firebase
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating user: " + error.message);
+    }
+  }
+
+  return userRef;
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
