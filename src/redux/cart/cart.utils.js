90dda9d90b93ae014deb59d56b68a93ab4fdfb2a
Gagan Singh <gagansingh.dev@outlook.com>
2021-12-09 19:49:51 -0500

    Implemented the ad and remove item logic to the checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   }
 };
+
+export const clearItemFromCart = (allCartItems, itemToClear) =>
+  allCartItems.filter((item) => item.id !== itemToClear.id);
+
+export const removeItemFromCart = (allCartItems, itemToRemove) => {
+  console.log("CLICKED REMOVE ITEM");
+  const existingItem = allCartItems.find((item) => item.id === itemToRemove.id);
+
+  if (existingItem.quantity === 1) {
+    // If the item has a quantity of 1 in the cart, remove it from the cart completely
+    return clearItemFromCart(allCartItems, itemToRemove);
+    // return allCartItems.filter((item) => item.id !== itemToRemove.id);
+  } else {
+    // Otherwise decrease quantity by 1
+    return allCartItems.map((item) =>
+      item.id === itemToRemove.id
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+  }
+};
