448093569e1086621478d88a25fa700a4488761a
Gagan Singh <gagansingh.dev@outlook.com>
2021-12-09 23:38:44 -0500

    Moved state from the Shop component to redux

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selectors.js";
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview.jsx";
-import SHOP_DATA from "./shop.data.js";
 
-export default class Shop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map((collection) => (
-          <CollectionPreview key={collection.id} {...collection} />
-        ))}
-      </div>
-    );
-  }
+function Shop({ collections }) {
+  return (
+    <div className="shop-page">
+      {collections.map((collection) => (
+        <CollectionPreview key={collection.id} {...collection} />
+      ))}
+    </div>
+  );
 }
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(Shop);
