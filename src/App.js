36779b0eed2bb75c30ba195d22ec35613b2f87f1
Gagan Singh <gagansingh.dev@outlook.com>
2021-12-14 14:54:58 -0500

    Converted from class component to function component and implemented useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -12,59 +12,32 @@ import SignInAndSignUpPage from "./pages/SignInAndSignUp/SignInAndSignUp";
 import CheckoutPage from "./pages/Checkout/Checkout";
 import "./App.css";
 
-// SEED SHOP DATA TO FIREBASE
-// import { addCollectionAndDocuments } from "./firebase/firebase.utils";
-// import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
-
-class App extends Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ currentUser, checkUserSession }) => {
+  useEffect(() => {
     checkUserSession();
+  }, [checkUserSession]);
 
-    // // SEED SHOP DATA TO FIREBASE
-    // addCollectionAndDocuments(
-    //   "collections",
-    //   this.props.collectionsArray.map(({ title, items }) => ({ title, items }))
-    // );
-    // });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    const { currentUser } = this.props;
-
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/checkout" exact component={CheckoutPage} />
-          <Route
-            path="/signin"
-            exact
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-// const mapStateToProps = ({ user: { currentUser } }) => ({
-//   currentUser,
-// });
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/checkout" exact component={CheckoutPage} />
+        <Route
+          path="/signin"
+          exact
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  // collectionsArray: selectCollectionsForPreview, // SEED SHOP DATA TO FIREBASE
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,37 +1,30 @@
-import React from "react";
+import { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 import CollectionsOverviewContainer from "../../components/CollectionsOverview/CollectionsOverview.container";
 import CollectionPageContainer from "../Collection/Collection.container";
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-    fetchCollectionsStart();
-  }
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+  useEffect(() => fetchCollectionsStart(), [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStart: (item) => dispatch(fetchCollectionsStart()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
